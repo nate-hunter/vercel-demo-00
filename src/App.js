62b48916a794976f6884554a02b3073ed46f20df
Nate <hunter.j.nate@gmail.com>
2021-01-29 00:41:49 -0500

    test again

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <p style={{ color: "#f99595" }}>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
